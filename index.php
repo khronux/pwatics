2db4461c382e7a8492a8b7ee90932aae6d66e7d1
Mr Kimu <hherrera@laselva.edu.mx>
2021-09-29 12:43:01 -0500

    "Primer snapshot de PWAtics"

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+
+	<head>
+	
+		<!-- 10A Ing DGS -->
+		<!-- Es importante aplicar UTF-8 para el uso de caracteres especiales -->
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    	<link rel="stylesheet" href="css/style.css">
+    	<link rel="manifest" href="manifest.json">
+    	<!-- ios support -->
+    	<link rel="apple-touch-icon" href="images/icons/icon-72x72.png">
+    	<link rel="apple-touch-icon" href="images/icons/icon-96x96.png">
+    	<link rel="apple-touch-icon" href="images/icons/icon-128x128.png">
+    	<link rel="apple-touch-icon" href="images/icons/icon-144x144.png">
+    	<link rel="apple-touch-icon" href="images/icons/icon-152x152.png">
+    	<link rel="apple-touch-icon" href="images/icons/icon-192x192.png">
+    	<link rel="apple-touch-icon" href="images/icons/icon-384x384.png">
+    	<link rel="apple-touch-icon" href="images/icons/icon-512x512.png">
+    	<meta name="apple-mobile-web-app-status-bar" content="#db4938">
+    	<meta name="theme-color" content="#db4938">
+    	<title>10A IGDS PWA</title>
+		
+	</head>
+
+  	<body>
+    	<main>
+      		<nav>
+        		<h1>Dev'Coffee</h1>
+        		<ul>
+          			<li>Home</li>
+          			<li>About</li>
+          			<li>Blog</li>
+        		</ul>
+      		</nav>
+      		<div class="container"></div>
+    	</main>
+    	<script src="js/app.js"></script>
+  	</body>
+</html>
